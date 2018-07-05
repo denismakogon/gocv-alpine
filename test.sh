6684c06436eccd5505908b8199170bdc854ef84e
Patrick McGee <patmcgee331@gmail.com>
2018-07-05 16:18:43 -0400

    Updated to use OpenCV 3.4.2 and gocv 0.14.0.

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-pushd build-stage && docker build -t denismakogon/gocv-alpine:3.4.1-buildstage .; popd
-pushd runtime && docker build -t denismakogon/gocv-alpine:3.4.1-runtime .; popd
+pushd build-stage && docker build -t denismakogon/gocv-alpine:3.4.2-buildstage .; popd
+pushd runtime && docker build -t denismakogon/gocv-alpine:3.4.2-runtime .; popd
 pushd example && docker build -t denismakogon/gocv-alpine:test .; popd
 docker run --rm -ti denismakogon/gocv-alpine:test
 docker rmi -f denismakogon/gocv-alpine:test
